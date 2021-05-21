e85fc080b21f5ef15b3580cac312ced5c9d63c56
Ryan Koschak <78557343+RyanKoschak@users.noreply.github.com>
2021-05-21 13:40:57 -0500

    Add files via upload

---
+++ assignment5Final/javascript.js
@@ -0,0 +1,21 @@
+// The web page has two background colors, Red and White. Has 3 links attached that are called: FAQs, Contacts, and Terms and Conditions. Lastly, the webpage has a lot of images on it. 
+function myfunction (){
+    //Can have more than two colors on my webpage that doesn't have to do with red or white.
+}
+function myfunction1 (){
+    //Can have more/less images if needed.
+}
+function myfunction2(){
+    document.addEventListener("click", myfunction1);
+}
+var facts = ["Questions", "Help", "Doctors"];
+
+if (degree < 50) {
+    doctor = "good"    
+}
+function myfunction3(){
+    document.getElementsByName("facts")
+}
+function myfunction4(){
+    document.images("img")
+}
